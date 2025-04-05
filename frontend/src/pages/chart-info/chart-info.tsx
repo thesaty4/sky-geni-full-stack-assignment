9368f817330ea0310ae4477f917d854f851016f0
Satya Mishra <Satya.Mishra@flydocs.aero>
2025-04-06 00:40:16 +0530

    fix: styling of the chart

--- frontend/src/pages/chart-info/chart-info.tsx
+++ frontend/src/pages/chart-info/chart-info.tsx
@@ -16,6 +16,9 @@ const ChartInfo = () => {
 
   return (
     <Box className="main__wrapper" component="div">
+      <Box className="header__wrapper" component="div">
+        Won ACV mix by Customer Type
+      </Box>
       <Box className="top__wrapper" component="div">
         <Box component="div">
           <D3Chart data={bar.data} chartType="bar" />
@@ -23,6 +26,16 @@ const ChartInfo = () => {
         <Box component="div">
           <D3Chart data={donut.data} chartType="donut" />
         </Box>
+      </Box>
+      <Box className="graph__footer__wrapper" component="div">
+        <div className="dot__wrapper">
+          <div className="dot first"></div>
+          <span className="footer__sub-title">Existing Customer</span>
+        </div>
+        <div className="dot__wrapper">
+          <div className="dot second"></div>
+          <span className="footer__sub-title">New Customer</span>
+        </div>
       </Box>
       <Box className="bottom__wrapper" component="div">
         <SharedTable headers={header} dataList={dataList} isFooterTotal />
