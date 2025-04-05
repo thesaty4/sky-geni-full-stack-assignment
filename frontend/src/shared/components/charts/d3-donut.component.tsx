1ea4a58657cec4a74b7cfbca95981acd4e472c03
Satya Mishra <Satya.Mishra@flydocs.aero>
2025-04-05 16:07:35 +0530

    fix: donut chart

--- frontend/src/shared/components/charts/d3-donut.component.tsx
+++ frontend/src/shared/components/charts/d3-donut.component.tsx
@@ -39,7 +39,7 @@ const DonutChart: React.FC<DonutChartProps> = ({ data }) => {
 
     const width = 500,
       height = 500,
-      radius = Math.min(width, height) / 3;
+      radius = Math.min(width, height) / 4;
 
     const svg = d3
       .select(chartRef.current) // selecting current chart ref
